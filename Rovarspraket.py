87d0e9479cf2d52cca7f3204d1d08a32b806cfec
Radu Vasilescu <vasilescur@gmail.com>
2017-07-24 22:39:48 -0400

    Update Rovarspraket.py

--- Rovarspraket.py
+++ Rovarspraket.py
@@ -1,3 +1,6 @@
+# Obfuscates text by replacing consonants with consonant + 'o' + consonant groups (Ex. "Hello" --> "Hohelollolo")
+# Source: https://www.reddit.com/r/dailyprogrammer/comments/341c03/20150427_challenge_212_easy_rövarspråket/
+
 for c in input():
     if c.lower() in "aeiouåäö" or not c.isalpha():
         print(c, end = "")
