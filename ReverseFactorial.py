612b8fe4eb9ea31a5df449937991a5b56ef5d3f3
Radu Vasilescu <vasilescur@gmail.com>
2017-07-24 22:38:26 -0400

    Update ReverseFactorial.py

--- ReverseFactorial.py
+++ ReverseFactorial.py
@@ -1,3 +1,6 @@
+# Computes the reverse factorial of a number (Ex. "24" --> "4!")
+# Source: https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/
+
 import math
 
 orig = int(input("Number: "))
