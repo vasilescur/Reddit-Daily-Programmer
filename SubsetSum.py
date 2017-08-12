f62b9083aaa21c8b2c6cbddce0b29b8205e97d36
Radu Vasilescu <vasilescur@gmail.com>
2017-08-12 08:42:57 -0400

    Update SubsetSum.py

--- SubsetSum.py
+++ SubsetSum.py
@@ -1,3 +1,6 @@
+# Determines whether there exists a pair of numbers in a list that add up to 0 (including 0).
+# Challenge: https://www.reddit.com/r/dailyprogrammer/comments/68oda5/20170501_challenge_313_easy_subset_sum/
+
 numbers = [int(x) for x in input().strip('[').strip(']').split(', ')]
 
 for num in numbers:
